bad6c52b78f8d1196c5eda6289899a10203700d6
Nivedita <niveditavb05@gmail.com>
2022-07-06 15:46:48 -0500

    6 july bits left shift operator

---
+++ bits left shift operator.cpp
@@ -0,0 +1,11 @@
+ #include<bits.stdc++.h>
+ using namespace std;
+ const intN=1e5+10;
+
+ int main(){
+    cout<<INT_MAX<<endl; //2147483647
+    int a=(1LL<<31)-1; //31 for signed cause 1bit is for sign
+    cout<<a<<endl;//2147483647
+    unsigned int b=(1LL<<32) -1; //32 for unsigned
+    cout<<b<<endl;
+     }
