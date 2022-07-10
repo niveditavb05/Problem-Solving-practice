21817b154cbffbdbb1bd3ae27389cc5da8677c2c
Nivedita <niveditavb05@gmail.com>
2022-07-09 20:11:12 -0500

    9 july

---
+++ Binary exponentiation recursive method.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int M 1e9+7;
+
+int binexprecur(int a,int b){
+    //base condition
+    if(b==0) return 1;
+    int   res=binexprecur(a,b/2);
+    if(b&1){
+       // return a*res*res;
+       return (a*((res*1LL*res)%M))%M;
+    }else{
+        //return res*res;
+        return(res*1LL*res)%M;
+    }
+}
+int main(){
+    int a=2,b=13;
+    cout<<binexprecur(a,b);
+    //int ans=1;
+    // for(int i=0;i<b;++i){
+    //     ans*=a;
+    //     ans%=M;
+    // } //O(n)
+   // cout<<ans<<endl;
+}
